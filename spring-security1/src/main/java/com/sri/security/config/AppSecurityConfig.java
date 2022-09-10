64a3fe26ff57c43bf6dc3e1146149a209eb17c85
Srikanth <srikanthc43@gmail.com>
2022-09-10 16:06:08 +0530

    Custom login and logout functionality

--- spring-security1/src/main/java/com/sri/security/config/AppSecurityConfig.java
+++ spring-security1/src/main/java/com/sri/security/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.sri.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,19 +12,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	// creating user credentials inside server memory
 	// username, password, role
-	
+
 	@Autowired
 	PasswordEncoder bcryPasswordEncoder;
 
-	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
 		auth.inMemoryAuthentication().withUser("srikanth")
-				.password("$2a$12$0gZAGFx4DfTRvnXHn4MCkeywLhLa8oHZQlyZTBuTsE8Idoe0FALxa").roles("admin");//$2a$12$0gZAGFx4DfTRvnXHn4MCkeywLhLa8oHZQlyZTBuTsE8Idoe0FALxa
-		//$2a$12$4qLu8nU0JwZACyx3QnmE3ucnDEliDMqtIif1bkb4VVmbxm/vpSKDy
-		System.out.println(bcryPasswordEncoder.encode("srikanth"));
+				.password("$2a$12$0gZAGFx4DfTRvnXHn4MCkeywLhLa8oHZQlyZTBuTsE8Idoe0FALxa").roles("admin").and()
+				.withUser("hari").password("$2a$10$bC80RhL7cQS6qpf2yJF/I.syWWaEVXjD3sRMlsIWGtG8mdUduOS2i")
+				.roles("user");// $2a$12$0gZAGFx4DfTRvnXHn4MCkeywLhLa8oHZQlyZTBuTsE8Idoe0FALxa
+		// $2a$12$4qLu8nU0JwZACyx3QnmE3ucnDEliDMqtIif1bkb4VVmbxm/vpSKDy
+
 	}
 
-	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/hello").authenticated().antMatchers("/bye").authenticated()
+				.antMatchers("/").permitAll().antMatchers("/login").permitAll().and().formLogin().loginPage("/login").loginProcessingUrl("/process-login").and().httpBasic().and().logout();
+	}
+
 }
--- spring-security1/src/main/webapp/WEB-INF/view/hello-world.jsp
+++ spring-security1/src/main/webapp/WEB-INF/view/hello-world.jsp
@@ -1,12 +1,33 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8" isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+	crossorigin="anonymous">
+<link rel="stylesheet"
+	href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+<!-- JavaScript Bundle with Popper -->
+<script
+	src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+	crossorigin="anonymous"></script>
+
+
 </head>
 <body>
 	<h1>Hello World</h1>
+	<form:form action="logout" method="post">
+		<button class="btn btn-outline-danger">logout</button>
+	</form:form>
+	
 </body>
 </html>
